c78037c2c12d3f19594386c3e17f69f6be20a529
SherryChen18 <earth18@gmail.com>
2021-02-18 15:08:09 +0800

    Update admin.py

--- erp/admin.py
+++ erp/admin.py
@@ -7,7 +7,7 @@ from .models import bomDefine, purchaseList, mpList
 from .models import customer, ccnList, software, endProduct
 
 
-#admin.site.register(pnCategory)
+admin.site.register(pnCategory)
 #admin.site.register(BomElement)
 #admin.site.register(QtyReason)
 #admin.site.register(planerElement)
