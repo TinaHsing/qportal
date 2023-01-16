e0e91967bf5d57e51c57bab3488dff644d3e4a4c
SherryChen18 <earth18@gmail.com>
2023-01-16 18:24:55 +0800

    Update admin.py

--- labwiki/admin.py
+++ labwiki/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
 from markdownx.admin import MarkdownxModelAdmin
 # Register your models here.
 
+admin.site.register(Category, MarkdownxModelAdmin)
 admin.site.register(Article, MarkdownxModelAdmin)
